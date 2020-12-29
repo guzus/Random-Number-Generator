61f74621f8aa4addeaf8d9f65ff558133a92943b
mgnlia <storminggalaxys4@gmail.com>
2020-12-29 22:30:25 +0900

    refactoring

--- popup.js
+++ popup.js
@@ -1,16 +1,26 @@
 let generate = document.getElementById('generate');
+let number = document.getElementById('number');
+let from = document.getElementById('from');
+let to = document.getElementById('to');
 
 // var explanation = document.getElementById('explanation');
 // explanation.innerText = "Generate Random among " + from + "~" + to;
 
 generate.onclick = function(element) {
-    var number = document.getElementById('number');
-    var from = document.getElementById('from').value;
-    var to = document.getElementById('to').value;
     number.innerText = Math.floor(
-        getRandom(parseInt(from,10), parseInt(to,10))
+        getRandom(parseInt(from.value,10), parseInt(to.value,10))
     );
 };
+
+to.addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      generate.click();
+    }
+  });
 
 function getRandom(min, max) {
     return Math.random()*(max-min+1) + min;
